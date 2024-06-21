9e4420ab1b7a065688bbb9811d66b716c95c9c62
Lucas <105462862+lucasalvisimader@users.noreply.github.com>
2024-06-21 14:49:09 -0300

    feat: added configuration icon to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 // css
 import './Header.css';
 
+// images
+import configuration_icon from '../../assets/images/configuration_icon.png';
+
 export const Header = () => {
 
     return (<>
@@ -9,6 +12,9 @@ export const Header = () => {
                 <li id='header_text_list_home'>Home</li>
                 <li id='header_text_list_search'>Search Challange</li>
                 <li id='header_text_list_about'>About Me</li>
+                <li id='header_text_list_configuration'>
+                    <img id='header_text_list_configuration_icon' src={configuration_icon}/>
+                </li>
             </ul>
         </div>
     </>);
