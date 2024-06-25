407db366bb318a89b9c64f430cdbe19fc66c26eb
Lucas <105462862+lucasalvisimader@users.noreply.github.com>
2024-06-25 15:27:05 -0300

    feat: started doing second challange

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import reportWebVitals from './reportWebVitals';
 import { Main } from './pages/main/Main';
 import { SearchResults } from './pages/search_results/SearchResults';
 import { MultiplesOf3Or5 } from './pages/challanges/multiples-of-3-or-5/MultiplesOf3Or5';
+import { EvenFibonacciNumbers } from './pages/challanges/even-fibonacci-numbers/EvenFibonacciNumbers';
 
 // components
 import { Header } from './components/header/Header';
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
     {
         path: 'search-results/multiples-of-3-or-5',
         element: <MultiplesOf3Or5 />
+    },
+    {
+        path: 'search-results/even-fibonacci-numbers',
+        element: <EvenFibonacciNumbers />
     }
 ])
 
--- src/pages/search_results/SearchResults.js
+++ src/pages/search_results/SearchResults.js
@@ -20,6 +20,11 @@ export const SearchResults = () => {
                 link: 'multiples-of-3-or-5', 
                 title: 'Multiples of 3 and 5', 
                 description: DOMPurify.sanitize('Find the sum of all the multiples of <b>3</b> or <b>5</b> below <b>1000</b>.')
+            }, {
+                id: 2, 
+                link: 'even-fibonacci-numbers', 
+                title: 'Even Fibonacci Numbers', 
+                description: DOMPurify.sanitize('Find the sum of the <b>even-valued</b> terms of Fibonacci.')
             }]);
     }, [navigate]);
 
@@ -27,15 +32,17 @@ export const SearchResults = () => {
         <div className='search_results'>
             <div className='search_results_challanges'>
                 <span className='search_results_challanges_found' id='search_results_challanges_found_title'>Found Challanges</span>
-                {challanges.map(challange => {
-                    return (<Fragment key={challange.id}>
-                        <div className='search_results_challange' key={challange.id} onClick={() => navigate(challange.link)}>
-                            <span className='search_results_challange_title' id={challange.id}>{challange.title}</span>
-                            <br/>
-                            <span className='search_results_challange_description' id={challange.id} dangerouslySetInnerHTML={{ __html: challange.description }}></span>
-                        </div>
-                    </Fragment>);
-                })}
+                <div className='search_result_challanges_map'>
+                    {challanges.map(challange => {
+                        return (<Fragment key={challange.id}>
+                            <div className='search_results_challange' key={challange.id} onClick={() => navigate(challange.link)}>
+                                <span className='search_results_challange_title' id={challange.id}>{challange.title}</span>
+                                <br/>
+                                <span className='search_results_challange_description' id={challange.id} dangerouslySetInnerHTML={{ __html: challange.description }}></span>
+                            </div>
+                        </Fragment>);
+                    })}
+                </div>
             </div>
         </div>
     </>);
